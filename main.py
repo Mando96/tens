8b0d75ba7eddd916a079253ee804a82d1b06d164
Oluwaseun Okeowo <oluwaseunokeowo@Oluwaseuns-Air.attlocal.net>
2023-10-24 03:49:05 -0400

    added a timer function

--- main.py
+++ main.py
@@ -1,33 +1,44 @@
 import random
+import time 
+from threading import Thread
 from itertools import islice
 
 tap = 0
 score = 0
-b = 4
+number_of_cards = 4
 
-#generate-random-numbers-within-a-given-range-and-store-in-a-list/
-def deck(start, end, num):
+
+ #Timer func
+def check():
+            time.sleep(5)
+            if tap < 0:
+                return
+            
+            print("Too Slow")
+
+#Generate random numbers within a given range and store in a list
+def para(start, end, num):
     res = []
  
     for j in range(num):
         res.append(random.randint(start, end))
- 
+
     return res
  
 # Driver Code
 num = 10
 start = 1
 end = 11
-print(deck(start, end, num))
+print(para(start, end, num))
 
-Input = deck(start, end, num)
+Input = para(start, end, num)
  
 # list of length in which we have to split 
 length_to_split = [5,5] 
  
 # Using islice 
 Inputt = iter(Input) 
-Output = [list(islice(Inputt, elem)) 
+deck = [list(islice(Inputt, elem)) 
         for elem in length_to_split] 
  
 # Printing Output 
@@ -36,26 +47,36 @@ Output = [list(islice(Inputt, elem))
 print ("     ")    
 print ("Welcome to Tens .mark1.\n**********************\n")
 
-while b >= 0:
-    indexx = random.randint(0,b)
-    indexy = random.randint(0,b)
+while number_of_cards >= 0:
+    
+    #Generate a random int to be used as index
+    index_x = random.randint(0,number_of_cards)
+    index_y = random.randint(0,number_of_cards)
 
-    x = Output[0][indexx]
-    y = Output[1][indexy]
+    #Retrive random number(card) from list(deck)
+    x = deck[0][index_x]
+    y = deck[1][index_y]
 
+    
+    print (f"Your cards: {x} and {y}")
+
+    Thread(target = check).start()
+
+    tap = int(input("Ten? => "))
+
+    #Add and Subtract cards 
     sum = x + y
     if x > y:
         sub = x - y
     else:
         sub = y -x
 
+    #Validate if sum or difference is multiple of 10
     if sum % 10 == 0 or sub % 10 == 0:
         val = True
     else:
         val = False
 
-    print (f"Your cards: {x} and {y}")
-    tap = int(input("Ten? => "))
     print ("     ")  
 
     if val == True and tap == 1:
@@ -67,14 +88,10 @@ while b >= 0:
     else:
         score = score
 
-    Output[0].pop(indexx)
-    Output[1].pop(indexy)
+    deck[0].pop(index_x)
+    deck[1].pop(index_y)
 
-    b -= 1
-
-    #print (b)
-    
-#print (f"sum = {sum} and sub = {sub}")
-#print (f"Is this a multiple of 10? {val}")
+    number_of_cards -= 1
 
 print (f"Your Score = {score}")
+print (tap)
